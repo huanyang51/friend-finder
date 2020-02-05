71c891742816b7c43349ce15de24e4996f0f3d60
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-05 16:04:58 -0500

    test to deploy

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
-var db = [];
-db.push({
-  name: "Ahmed",
-  photo:
-    "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-});
+var db = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
