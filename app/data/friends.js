3328aa970562c7c43b349125e9c6ebe92460f25a
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-09 10:54:01 -0500

    feat: change to display the image in the modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@ var db = {
       name: "Ahmed",
       photo:
         "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
       name: "Jacob Deming",
